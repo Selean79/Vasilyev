972e37515aa5c147a372ed7a50bacff8ca98e763
Selean79 <seleznjov@gmail.com>
2020-12-21 00:28:26 +0200

    Push exc 1 and 2

---
+++ Overloading/Exc_1/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace Exc_1
+{
+    class MyClass
+    {
+        char ch;
+
+        public MyClass(char c)
+        {
+            ch = c;
+        }
+
+        public static MyClass operator ++(MyClass ob)
+        {
+            ob.ch = (char)(ob.ch + 1);
+            return ob;
+        }
+
+        public static MyClass operator --(MyClass ob)
+        {
+            ob.ch = (char)(ob.ch - 1);
+            return ob;
+        }
+
+        public static MyClass operator +(MyClass ob, int n)
+        {
+            ob.ch = (char)(ob.ch + n);
+            return ob;
+        }
+
+        public static MyClass operator +(int n, MyClass ob)
+        {
+            return ob + n;
+        }
+
+        public static int operator -(MyClass ob1, MyClass ob2)
+        {
+            return (int)(ob1.ch) - (int)(ob2.ch);
+        }
+
+
+        public override string ToString()
+        {
+            string txt = "char: " + ch;
+            return txt;
+        }
+    }
+
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass ob1 = new MyClass('a');
+            MyClass ob2 = new MyClass('s');
+            ob1++;
+            Console.WriteLine(ob1);
+            ob1--;
+            Console.WriteLine(ob1);
+            Console.WriteLine(ob1 + 3);
+            Console.WriteLine(5 + ob1);
+
+            Console.WriteLine(new String('-', 10));
+            Console.WriteLine(ob2);
+          
+            Console.WriteLine(ob1);
+            Console.WriteLine(ob2 - ob1);
+            Console.WriteLine(ob1 - ob2);
+        }
+    }
+}
---
+++ Overloading/exc_2/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+
+namespace exc_2
+{
+    class MyClass
+    {
+        private int[] myArray;
+
+        public MyClass(int n)
+        {
+            myArray = new int[n];
+        }
+
+        public static string operator ~(MyClass ob)
+        {
+            string s = null;
+            foreach (int k in ob.myArray)
+            {
+                s += (string)(k + " ");
+            }
+
+            return s;
+        }
+
+        public static MyClass operator ++(MyClass ob)
+        {
+            MyClass ob2 = new MyClass(ob.myArray.Length + 1);
+            // ob = ob2;
+            return ob2;
+        }
+
+        public static MyClass operator --(MyClass ob)
+        {
+            MyClass ob2 = new MyClass(ob.myArray.Length - 1);
+            //  ob = ob2;
+            return ob2;
+        }
+
+        public static MyClass operator +(MyClass ob, MyClass ob1)
+        {
+            MyClass ob2 = new MyClass(ob.myArray.Length + ob1.myArray.Length);
+            //ob = ob2;
+            return ob2;
+        }
+
+         public static MyClass operator +(MyClass ob, int n)
+        {
+            MyClass ob1 = new MyClass(ob.myArray.Length + n);
+            ob1.myArray[ob1.myArray.Length - 1] = n;
+            return ob1;
+        }
+
+        public static MyClass operator +(int n, MyClass ob)
+        {
+            MyClass ob1 = new MyClass(ob.myArray.Length + n);
+            ob1.myArray[0] = n;
+            return ob1;
+        }
+
+
+        public override string ToString()
+        {
+            string s = null;
+            foreach (int i in myArray)
+            {
+                s += i + " ";
+            }
+            return s;
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass ob = new MyClass(2);
+            MyClass ob1 = new MyClass(5);
+            Console.WriteLine(ob);
+            Console.WriteLine(~ob);
+            Console.WriteLine(++ob);
+            Console.WriteLine(++ob);
+            Console.WriteLine(--ob);
+            Console.WriteLine(new String('-', 10));
+            Console.WriteLine(ob + ob1); 
+            MyClass ob3 = new MyClass(2);
+            MyClass ob4 = new MyClass(2);
+            Console.WriteLine(5 + ob3);
+            Console.WriteLine(ob4 + 10);
+
+        }
+    }
+}
