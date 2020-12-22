fc90326a89d388960f4513f685500548d756369d
Andrew Seleznjov <Seleznjov@gmail.com>
2020-12-23 01:05:26 +0200

    Add new Project

---
+++ Property/Property/BasicProperty/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace BasicProperty
+{
+    class MyClass
+    {
+        private int num;
+        private int min;
+        private int max;
+
+        public MyClass(int a, int b)
+        {
+            if (a > b)
+            {
+                min = a;
+                max = b;
+            }
+            else
+            {
+                min = b;
+                max = a;
+            }
+            
+            num = (max + min) / 2;
+        }
+
+        public int Code
+        {
+            get
+            {
+                return num;
+            }
+            set
+            {
+                if (value < min)
+                {
+                    num = min;
+                }
+                else if(value > num)
+                {
+                    num = max;
+                }
+                else
+                {
+                    num = value;
+                }
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass ob = new MyClass(10, 9);
+            Console.WriteLine($"Property Code: {ob.Code}");
+            ob.Code = 7;
+            Console.WriteLine($"Property Code: {ob.Code}");
+            ob.Code = 20;
+            Console.WriteLine($"Property Code: {ob.Code}");
+            ob.Code = -10;
+            Console.WriteLine($"Property Code: {ob.Code}");
+        }
+    }
+}
---
+++ Property/Property/UsingProperty/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace UsingProperty
+{
+    class Program
+    {
+        class MyClass
+        {
+            private int first;
+            private int last;
+
+            public MyClass(int a, int b)
+            {
+                first = a;
+                last = b;
+            }
+
+            public override string ToString()
+            {
+                string txt = "object's fields: ";
+                txt += first + " and " + last;
+                return txt;
+            }
+
+            public int Number
+            {
+                get
+                {
+                    int t = last;
+                    last = first;
+                    first = t;
+                    return t;
+                }
+
+                set
+                {
+                    last = first;
+                    first = value;
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            MyClass ob = new MyClass(1, 9);
+            Console.WriteLine(ob);
+            Console.WriteLine("Property Number: " + ob.Number);
+            Console.WriteLine(ob);
+            ob.Number = 5;
+            Console.WriteLine(ob);
+            Console.WriteLine("Property Number: " + ob.Number);
+            Console.WriteLine(ob);
+        }
+    }
+}
